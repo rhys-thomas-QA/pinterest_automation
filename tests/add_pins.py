5e65f10c140ab6db8e4855c9ce713e0edc51ae03
Rhys Thomas <53527841+rhys-thomas-QA@users.noreply.github.com>
2020-06-25 18:32:16 +1200

    Update add_pins.py

--- tests/add_pins.py
+++ tests/add_pins.py
@@ -11,19 +11,7 @@ title_array = []
 results_array = []
 dashed_lowercase_artist = ""
 headers = {
-    'authority': 'www.pinterest.co.uk',
-    'accept': 'application/json, text/javascript, */*, q=0.01',
-    'x-pinterest-appstate': 'active',
-    'x-app-version': 'a2a392a',
-    'x-requested-with': 'XMLHttpRequest',
-    'x-pinterest-experimenthash': '8ce911ccc491eeaff4cd32514fc52d66c7f980b0d9f5cbff9ec37ab242afc3f527b2c9e0b71af1574b7b6c3ff0710bca87328e0af67802887d9278b546134edc',
-    'user-agent': 'Chrome/81.0.4044.138',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://www.pinterest.co.uk/',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cookie': 'csrftoken=34d8c022b199d4258a6f48e00a1a9aa3; bei=false; cm_sub=denied; logged_out=True; fba=True; g_state={"i_p":1588835383497,"i_l":2}; ujr=1; fbm_274266067164=base_domain=.www.pinterest.co.uk; _auth=1; _pinterest_sess=TWc9PSZ1WUxsbE5zaW1qOUltc1ppV2d3UTZ2emdBRGlOdEJ4dlptU2dvMHB2SlIzN01rVDlxcEFna21ocWNoMGZJT2dBZjR4Qk5yTVgwRlVUR2R2enNHRlVWRnBFYWtGYnkwK3VmZTJ3K2JHakZJMXNUeEZHVHFFaFE4ZFpyZy9FRjBGNnB2S0JEZCtsd3ZhL0xyazBWd0I3d3dJRFRORTFUOHJrYUxmTjF2NnMvK1l0UmZlM004NEdaTi9MTDJLalg5RFY4SG5vV05Md2hNdGJJZHFWelVsTTZyUDlRYld1SmUxZkh0eG9RSitoMGh4VGpWcGVkUGprV1pnRVFmL3hlOWQ4L2JueFd4dnZqZzUxd2FxUDVZTVNCd3lDVS9aZ2VkRlRlOU1WSC8zWTIwWkNtNnhGMHk0RUgzV0hIVURWOGNyZ00rcHd3VExtMmZWeFFxNk02bE1WVklJQnZlMVBHZWFkVEhyRHlrU2dXdmtaTzkvQmlGd050RlNZYzFWV1N5bThWdWs1cGNsS0xLQW5CUWoxTHpuQ1VNQWVtaFVtdGROWHEwcHpySmVrb09uTEdKYkFoTnA3cEo0THNMT25nWWYxejliQTY5M2xvbENUNHpVclNLT05rT2gxMlJTYWtjYTRnVGgycTJ6SmdIWWVOV2U1QjZxbEFHUVNrU1FMbk15VlptVHU2VVVFNXZ4ZGdhRXBHcTB6U2pzVU9sV3JYMjJBRnNwWVd2czQ2K1ovOHdtVnBRUThYWkh4WjlzdE5pUjNzUVBKWldNM21SaWp1akJ5YmFkSjFuVnlDV1JtbzRLUzZxVm1pSkNkaVN2RkFDQi9mY3R1bXczRVArZFYxS1JtNHJYaVFqSmZxaHg2TGVkOE9wYjl2SkpYU2JPRnZBYkhteTlFNGptOXBmRjR4dlJTN0VJZWNBUTdhSUV1elowV3ZYU0dDeUVpOWZ0YitoSUdYQWQyWVVvWmNlT3Q1K0pRNnZOcWYwWlJDVm5YSWNtcTQ1a2NjNGRBTG1hdmhSOE40QnZCN1pUR2ZmVVJGcHJLTXN6Y2xKeGN3ZHpwVWlxMkVSY2g4V1Y2cGlYTUMrT1RsNTFSZytOVTVFeE84Q2hVdW9PcUZMM3VpS3JjWGRVeW0yQWhuTklBNDlaTzhPbnF2aDJyOGxBb1FicCtlN1Qvc2l1UHpJWWdHeDhaaXBHNUtIQUpTZ0hsamdKVkpoNkRjb3hOR3Z3a1YrQVFMYzd3SFFmRzZMTTJVdTZBM2IwNUsyRnVzNkJnM3JqNVVvUFhrSnhoQTQ1Wk9sWXJORHcyZ25BMWxyWGFPT2VWUnZsTVJ2S3dOZGE3ZysvbjAyaHdLcHozSUFRbXJrOEl2ODZpNThKdGFRcXB3MUx5VGd2dzBadmsrQ2tQbFE3RytBc2VYd0Z5aTd6RzVnREI3U2NiZVQxTk9jMjh6eXErbHdNZTN4TzdyelVGNCszY3F6TTIrUFRHMUU4aWVTSzZVbStXZ2tLZ21Va0E3VmlTSlFKN0pqRmlURGFnaTlXY3gxaERKSXRYeDA3R25oZDVWUW9OYmhYaFdHZ3dScCtmek1EUzJkcGF3bzIwa29lcHloMGQ2ZW12YjhWZXFlUmdhWTFka0R1WFVOSUdpQlYvYU9jdWdYSDF3elVLd3c2R25UZFMxT3hDSnF3SUFPVzNPc1JaeTJoR1psZlhpdk0yTzNyM0Z6TVpxalE2WUtlcjRjWVdsZ3J0V3JGY1RqN0FtRWF5K0lrTHo0TUk3R3EyUU12eTJFSkVCM1NubDIzQnN6S2VzK0R3OEpOdEwwSXBOTFlwNm1YdS9HYnhhN2ZLMjlUQ0xKTHJRTWx0OWVHaFVEMDhTQmxVQ083WWlpYmYrbVpZJnVhM3BLN0dYL2NYdlFmb1drQXEvM2h2WHNxcz0=; _routing_id="11b53b90-db40-4e10-9b81-b511e4736845"; sessionFunnelEventLogged=1'
+#removed for security purposes
 }
 payload = {}
 base_url = "https://www.pinterest.co.uk/resource/"
